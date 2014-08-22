30ca35d4ecae0b54c27190f57865a9abe0020874
Brian Carroll <bcarroll@squarespace.com>
2014-08-22 13:24:00 -0400

    Added ImageLoader to site.js for built in image resizing

--- scripts/site.js
+++ scripts/site.js
@@ -1,17 +1,43 @@
 /*
-  Helpful Links
+  Create a new YUI instance and load the node
+  and event modules.
+
+  HELPFUL LINKS
   =============
 
-  https://yuilibrary.com/yui/docs/
+  http://yuilibrary.com/yui/docs/api/
   http://www.jsrosettastone.com/
-  http://developers.squarespace.com/custom-javascript/
 */
 
-
-// Create a YUI sandbox on your page.
-YUI().use('node', 'event', function (Y) {
+YUI().use('node', 'event', 'squarespace-util', function (Y) {
   Y.on('domready', function () {
-    // The Node and Event modules are loaded and ready to use.
-    // Your code goes here!
+
+    /*
+      This function loads and refreshes images
+      using Squarespace's responsive ImageLoader.
+    */
+
+    function loadImages () {
+      Y.all('img[data-src]' ).each(function(img) {
+        ImageLoader.load(img);
+      });
+    }
+
+    /*
+      ImageLoader will refresh images on windowresize
+      and any change in the Style Editor.
+    */
+
+    Y.one(window).on('resize', loadImages, this);
+    Y.Global && Y.Global.on('tweak:change', loadImages, this);    
+
+
+
+    /*
+      Author's custom code goes here.
+      =============================== */
+
+
+
   });
 });
